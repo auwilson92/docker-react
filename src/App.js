448abd2c6fc9be1a6e444a3a159462a912b6b161
auwilson92 <auwilson92@gmail.com>
2020-11-17 20:20:22 -0800

    add comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sup Girl
+          Sup Girlllll
         </p>
         <a
           className="App-link"
